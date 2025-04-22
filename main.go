49afdad5cde6a9dba4c93d7a0fceedf9c912d0b7
michabay05 <michabay05@gmail.com>
2025-04-21 22:32:04 -0400

    Parsed through array of candles
    Also: removed unnecessary printing to test API access and changed API
    URL to remove optional parameters

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ import (
 )
 
 const BASE_URL = "https://api.polygon.io"
-const TICKET_ENDPT = "/v3/reference/tickers"
+
+type CandleBar struct {
+	open      float64
+	close     float64
+	low       float64
+	high      float64
+	volume    int
+	timestamp uint64
+}
 
 func main() {
 	bytes, err := os.ReadFile("API_KEY.secret")
@@ -21,17 +29,15 @@ func main() {
 	API_KEY := strings.Split(content, "\n")[0]
 
 	stock_ticker := "AAPL"
-	mult := 2
+	mult := 5
 	timespan := "hour"
-	from := "2024-09-09"
-	to := "2024-09-11"
-	limit := 120
+	from := "2024-01-10"
+	to := "2024-01-13"
 	target_url := fmt.Sprintf(
-		"%s/v2/aggs/ticker/%s/range/%d/%s/%s/%s?apiKey=%s&limit=%d",
-		BASE_URL, stock_ticker, mult, timespan, from, to, API_KEY, limit,
+		"%s/v2/aggs/ticker/%s/range/%d/%s/%s/%s?apiKey=%s",
+		BASE_URL, stock_ticker, mult, timespan, from, to, API_KEY,
 	)
-
-	fmt.Println("Target URL: " + target_url + API_KEY)
+	fmt.Println("Target URL: " + target_url)
 
 	resp, err := http.Get(target_url)
 	if err != nil {
@@ -47,29 +53,21 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(root)
 
-	fmt.Printf("      Ticker: %s\n", root.(Object)["ticker"].(string))
-	fmt.Printf(" Query count: %d\n", int(root.(Object)["queryCount"].(float64)))
-	count := int(root.(Object)["resultsCount"].(float64))
-	fmt.Printf("Result count: %d\n", int(root.(Object)["resultsCount"].(float64)))
-	fmt.Printf("    Adjusted: %t\n", root.(Object)["adjusted"].(bool))
-
+	// TODO: use `json.Unmarshal()` instead of iterating through the array
 	results := root.(Object)["results"].(Array)
-	for i := range count {
-		c := results[i].(Object)
-		fmt.Printf("\t v: %d\n", int(c["v"].(float64)))
-		fmt.Printf("\tvw: %f\n", c["vw"].(float64))
-		fmt.Printf("\t o: %f\n", c["o"].(float64))
-		fmt.Printf("\t o: %f\n", c["o"].(float64))
-		fmt.Printf("\t c: %f\n", c["c"].(float64))
-		fmt.Printf("\t h: %f\n", c["h"].(float64))
-		fmt.Printf("\t l: %f\n", c["l"].(float64))
-		fmt.Printf("\t t: %d\n", int64(c["t"].(float64)))
-		fmt.Printf("\t n: %d\n", int(c["n"].(float64)))
+	candles := []CandleBar{}
+	for _, candle := range results {
+		c := candle.(Object)
+
+		cnd := CandleBar{
+			low:       c["l"].(float64),
+			open:      c["o"].(float64),
+			close:     c["c"].(float64),
+			high:      c["h"].(float64),
+			timestamp: uint64(c["t"].(float64)),
+			volume:    int(c["v"].(float64)),
+		}
+		candles = append(candles, cnd)
 	}
-	fmt.Printf("      Status: %s\n", root.(Object)["status"].(string))
-	fmt.Printf("  Request ID: %s\n", root.(Object)["request_id"].(string))
-	fmt.Printf("        Count: %d\n", int(root.(Object)["resultsCount"].(float64)))
-	// fmt.Printf("     Next URL: %s\n", root.(Object)["next_url"].(string))
 }
